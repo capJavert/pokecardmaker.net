aab79de5f3e7658105f99cd6c99f0e776ef4018c
Leon van der Noll <leonvdnoll@gmail.com>
2022-10-03 21:38:59 +0200

    working on getting name width responsive

--- src/features/cardEditor/cardStyles/components/blocks/NameBar/fields/Name/index.tsx
+++ src/features/cardEditor/cardStyles/components/blocks/NameBar/fields/Name/index.tsx
@@ -1,18 +1,65 @@
 import { useCardOptions } from '@cardEditor/cardOptions';
 import { useCardStyles } from '@cardEditor/cardStyles';
-import { FC } from 'react';
+import { Box } from '@mui/system';
+import { FC, useMemo, useRef } from 'react';
+import { getNameSymbolSize } from '../NameSymbol/utils';
 import { NameText } from './styles';
 
 const Name: FC = () => {
   const { name } = useCardOptions();
-  const { nameOutline, nameTextColor } = useCardStyles();
+  const { nameOutline, nameTextColor, nameSymbol } = useCardStyles();
+  const ref = useRef<HTMLDivElement | null>(null);
+  const invisibleRef = useRef<HTMLDivElement | null>(null);
+
+  const maxWidth = useMemo(() => {
+    // TODO: Calculate
+    return 215;
+  }, []);
+
+  const nameSymbolSize = useMemo(
+    () => getNameSymbolSize(nameSymbol)?.width || 0,
+    [nameSymbol],
+  );
+
+  const scale = useMemo<number>(() => {
+    // TODO: Only works for max size, not responsive. Calculate based of CardDisplay font size
+    const width = invisibleRef.current?.getBoundingClientRect().width || 0;
+
+    return Math.min(maxWidth / width, 1);
+  }, [maxWidth, name]);
 
   if (!name) return null;
 
   return (
-    <NameText textOutline={nameOutline} textColor={nameTextColor}>
-      {name}
-    </NameText>
+    <>
+      <Box
+        ref={ref}
+        sx={{
+          transformOrigin: 'left center',
+          transform: `scale(${scale}, 1)`,
+          width: scale === 1 ? 'unset' : `calc(100% - ${nameSymbolSize})`,
+        }}
+      >
+        <NameText textOutline={nameOutline} textColor={nameTextColor}>
+          {name}
+        </NameText>
+      </Box>
+      <Box
+        ref={invisibleRef}
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: -100,
+          opacity: 0,
+          pointerEvents: 'none',
+        }}
+      >
+        <NameText textOutline={nameOutline} textColor={nameTextColor}>
+          {name}
+        </NameText>
+      </Box>
+    </>
   );
 };
 
---
+++ src/features/cardEditor/cardStyles/components/blocks/NameBar/fields/NameSymbol/utils.ts
@@ -0,0 +1,53 @@
+import { NameSymbol } from '@cardEditor/cardStyles/types';
+
+export const getNameSymbolSize = (
+  nameSymbol?: NameSymbol,
+):
+  | {
+      width: string;
+      height: string;
+      top?: string;
+    }
+  | undefined => {
+  switch (nameSymbol) {
+    case 'v':
+      return {
+        height: '3.25em',
+        width: '4.5em',
+      };
+    case 'vmax':
+      return {
+        height: '3.25em',
+        width: '6.6em',
+      };
+    case 'vstar':
+      return {
+        height: '4em',
+        width: '7em',
+      };
+    case 'star':
+      return {
+        height: '3em',
+        width: '5.5em',
+      };
+    case 'ex':
+      return {
+        height: '3em',
+        width: '4.2em',
+      };
+    case 'gx':
+    case 'gxUltraBeast':
+      return {
+        height: '3em',
+        width: '6.8em',
+        top: '4%',
+      };
+    case 'prismStar':
+      return {
+        height: '3em',
+        width: '2.3em',
+      };
+    default:
+      return undefined;
+  }
+};

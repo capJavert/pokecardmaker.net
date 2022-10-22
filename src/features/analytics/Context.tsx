2e2ed29f18e8ffb4ff68ccdf97a7e13e0852b0df
Leon van der Noll <leonvdnoll@gmail.com>
2022-10-22 17:02:32 +0200

    fixed theme analytics

--- src/features/analytics/Context.tsx
+++ src/features/analytics/Context.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useCallback, useEffect, useRef } from 'react';
 import { useCardOptions, useCardRelations } from '@cardEditor/cardOptions';
+import { useSettings } from '@features/settings';
 import { pushToDataLayer, relationsToSlugs } from './utils';
 import { AnalyticsEvent, CardCreatorAnalyticsEvent } from './types';
 
@@ -17,6 +18,7 @@ export const AnalyticsContext = createContext<AnalyticsContextInterface>({
 });
 
 export const AnalyticsProvider: React.FC = ({ children }) => {
+  const { themeMode } = useSettings();
   const { retreatCost } = useCardOptions();
   const { typeImg: _, ...relations } = useCardRelations();
   const cardDataRef = useRef({
@@ -31,16 +33,18 @@ export const AnalyticsProvider: React.FC = ({ children }) => {
     };
   }, [retreatCost, relations]);
 
+  // TODO: Maybe only track relations that have dependencies with this fn (so not weaknessType, badgeIcon, etc.)
   const trackCardCreatorEvent = useCallback(
     (event: CardCreatorAnalyticsEvent, data?: Record<string, string>) => {
       setTimeout(() => {
         pushToDataLayer(event, {
           card: cardDataRef.current,
+          theme: themeMode,
           ...data,
         });
       }, 1000);
     },
-    [],
+    [themeMode],
   );
 
   const trackEvent = useCallback(

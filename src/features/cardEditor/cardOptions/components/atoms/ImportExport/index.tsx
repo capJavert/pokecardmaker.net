e9e2665913f448bd490f46ebef1c04ed772bca22
capJavert <ante@kickass.website>
2023-04-26 15:31:10 +0200

    feat: expand experimental config import by default

--- src/features/cardEditor/cardOptions/components/atoms/ImportExport/index.tsx
+++ src/features/cardEditor/cardOptions/components/atoms/ImportExport/index.tsx
@@ -1,4 +1,3 @@
-import { useCardDebugStore } from '@cardEditor/cardDebug';
 import ControlledCheckbox from '@components/inputs/ControlledCheckbox';
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
@@ -7,8 +6,7 @@ import ExportButton from './atoms/ExportButton';
 import ImportButton from './atoms/ImportButton';
 
 const ImportExport: FC = () => {
-  const showDebug = useCardDebugStore(store => store.showDebug);
-  const [active, setActive] = useState<boolean>(showDebug);
+  const [active, setActive] = useState<boolean>(true);
 
   return (
     <Box display="flex" flexDirection="column" gap={1}>
@@ -22,7 +20,10 @@ const ImportExport: FC = () => {
         <>
           <Typography variant="caption">
             EXPERIMENTAL: This allows you to import and export your card as a
-            JSON-object to or from your clipboard
+            JSON-object to or from your clipboard.
+            <br />
+            You can also pass the URL to your JSON config as <b>?url</b> query
+            param.
           </Typography>
           <Box
             display="flex"
